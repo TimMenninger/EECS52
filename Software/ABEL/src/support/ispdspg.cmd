85f56fe92c28c5daf40fe7c5616d3af45e1f9b5c
Tim Menninger <tmenninger@caltech.edu>
2016-11-03 10:32:36 -0700

    deleted garbage

--- Software/ABEL/src/support/ispdspg.cmd
+++
@@ -1,1 +0,0 @@
-C:\ispLEVER_Classic2_0\ispcomp\bin\dpm.exe -i "c:\users\tim\desktop\classes\eecs52\software\abel\src\support\ints.laf" -if laf -p ispLSI1016EA-125LJ44 -of vhdl -of verilog -s d -e 2 -z -m 16 -n 4 -ta on -y "c:\users\tim\desktop\classes\eecs52\software\abel\src\support\ints.xpn" -r "c:\users\tim\desktop\classes\eecs52\software\abel\src\support\ispxpert.par" 
